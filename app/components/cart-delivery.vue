6e3a6c6be9313a342d4d7c5bcdb24888046dc14d
Matthijs Alles <admin@bixie.nl>
2016-12-07 20:41:22 +0100

    allow chaining

--- app/components/cart-delivery.vue
+++ app/components/cart-delivery.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="uk-form">
 
-        <ul v-if="$bixCart.cart.delivery_options.length && !$bixCart.saving" class="uk-list uk-list-line">
-            <li v-for="delivery_option in $bixCart.cart.delivery_options">
+        <ul v-if="!$bixCart.saving" class="uk-list uk-list-line">
+            <li v-for="delivery_option in deliveryOptions.options">
                 <label class="uk-flex uk-flex-middle uk-flex-space-between">
                     <div class="uk-margin-right">
-                        <input type="radio" v-model="$bixCart.cart.delivery_option_id" :value="delivery_option.id"/>
+                        <input type="radio" v-model="$bixCart.cart.delivery_option_id" :value="delivery_option.option_id"/>
                     </div>
                     <div class="uk-flex-item-1">
-                        <strong>{{ getDaysFormat(delivery_option.business_days) }}</strong> - {{ delivery_option.eta_date | date}}
+                        <strong>{{ getDaysFormat(delivery_option.business_days) }}</strong> - {{
+                        delivery_option.eta_date | date}}
                         <span v-if="delivery_option.price"><br>{{{ {price: delivery_option.price} | productprice }}}</span>
                     </div>
                 </label>
             </li>
+            <li v-for="message in deliveryOptions.messages" class="uk-text-primary">{{ message }}</li>
+            <li v-for="error in deliveryOptions.errors" class="uk-text-danger">{{ error }}</li>
         </ul>
         <p v-else class="uk-text-center"><i class="uk-icon-spinner uk-icon-spin"></i></p>
 
@@ -39,25 +42,32 @@
         created: function () {
             this.$bixCart = this.$root;
             this.checkValue();
-            this.$watch('$bixCart.cart.delivery_options', this.checkValue);
+            this.$watch('$bixCart.cart.deliveryOptions.options', this.checkValue);
         },
 
         computed: {
             valid() {
                 return !!this.$bixCart.cart.delivery_option_id;
+            },
+            countOptions() {
+                return $bixCart.cart.deliveryOptions.options && $bixCart.cart.deliveryOptions.options.length;
+            },
+            deliveryOptions() {
+                return $bixCart.cart.deliveryOptions;
             }
         },
 
         methods: {
             checkValue() {
-                if (this.$bixCart.cart.delivery_options.length) {
-                    var current = _.find(this.$bixCart.cart.delivery_options, {id: this.$bixCart.cart.delivery_option_id});
+                if (this.countOptions) {
+                    var current = _.find(this.deliveryOptions.options, {option_id: this.$bixCart.cart.delivery_option_id});
                     if (!current) {
-                        if (!this.$bixCart.cart.delivery_options.length) {
-                            return this.$bixCart.save();
-                        }
-                        this.$bixCart.cart.delivery_option_id = _.first(this.$bixCart.cart.delivery_options).id
+                        this.$bixCart.cart.delivery_option_id = _.first(this.deliveryOptions.options).option_id
                     }
+                }
+                //not loaded yet?
+                if ($bixCart.cart.deliveryOptions.options === undefined) {
+                    this.$bixCart.saveCart();
                 }
 
             },
